70b6f09593c3bd3001301a1a8aaf428c7c615f25
VIJAY KUMAR TYAGI <vijaytyagi1998@gmail.com>
2020-03-31 22:21:01 +0530

    Update Student.java

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -1,10 +1,53 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.student;
-// to store student information and properties
-public class Student {
+
+public class Student implements Comparable<Student> {
+    private int rollNo;
+    private String firstName;
+    private String lastName;
+    private int backLog;
+
+    public Student(int rollNo, String firstName, String lastName, int backLog) {
+        this.rollNo = rollNo;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.backLog = backLog;
+    }
+    public int getBackLog() {
+        return backLog;
+    }
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    @Override
+    public String toString() {
+        return "RollNo:-"+ this.getRollNo() +" Name:- "+this.getFirstName()+" "+this.getLastName()
+                +"backLogs"+this.getBackLog();
+    }
+
+    @Override
+    public int compareTo(Student student) {
+        return Integer.compare(this.rollNo, student.rollNo);
+    }
 }
