151b76fa1f3b52277aa0b38d6cdfadf0b1392945
VIJAY KUMAR TYAGI <vijaytyagi1998@gmail.com>
2020-03-31 22:16:00 +0530

    Update MyMain.java

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -2,10 +2,44 @@
  *  Created by IntelliJ IDEA.
  *  User: Vaibhav
  *  Date: 23-Mar-20
- *  Time: 7:08 PM
+ *  Time: 7:13 PM
  */
-package problem4.main;
-// executable class
-// use problem1.mybst.MyBinarySearchTree class for creating binary tree
-public class MyMain {
+package problem3.node;
+
+
+import problem5.student.Student;
+
+public class Node {
+    private Student data;
+    private  Node next;
+
+    public Node(Student data) {
+        this.data = data;
+        this.next= null;
+    }
+
+    public Student getData() {
+        return data;
+    }
+
+    public void setData(Student data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node(Student data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+    @Override
+    public String toString() {
+        return this.getData().toString();
+    }
 }
