2b89a8b49d1aff5f7305af712ecb667c8f789177
VIJAY KUMAR TYAGI <vijaytyagi1998@gmail.com>
2020-03-31 22:14:42 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -6,5 +6,40 @@
  */
 package problem3.node;
 
+
+import problem5.student.Student;
+
 public class Node {
+    private Student data;
+    private  Node next;
+
+    public Node(Student data) {
+        this.data = data;
+        this.next= null;
+    }
+
+    public Student getData() {
+        return data;
+    }
+
+    public void setData(Student data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node(Student data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+    @Override
+    public String toString() {
+        return this.getData().toString();
+    }
 }
